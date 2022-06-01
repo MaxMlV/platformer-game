af97cfc93bfa20a77aaeb75f961d252ae9553110
MaxMlV <max.mlv.k@gmail.com>
2022-06-01 21:59:05 +0300

    initial commit

---
+++ player.py
@@ -0,0 +1,41 @@
+import pygame
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, pos):
+        super().__init__()
+        self.image = pygame.Surface((32,64))
+        self.image.fill('yellow')
+        self.rect = self.image.get_rect(topleft = pos)
+
+        # player movement
+        self.direction = pygame.math.Vector2(0,0)
+        self.speed = 4
+        self.gravity = 0.8
+        self.jump_height = -16
+
+    def get_input(self):
+        keys = pygame.key.get_pressed()
+
+        # move (left, right)
+        if keys[pygame.K_RIGHT]:
+            self.direction.x = 1
+        elif keys[pygame.K_LEFT]:
+            self.direction.x = -1
+        else:
+            self.direction.x = 0
+
+        # jump
+        if keys[pygame.K_SPACE]:
+            self.jump()
+
+    def apply_gravity(self):
+        self.direction.y += self.gravity
+        self.rect.y += self.direction.y
+
+    def jump(self):
+        self.direction.y = self.jump_height
+
+    def update(self):
+        self.get_input()
+        self.rect.x += self.direction.x * self.speed
+        self.apply_gravity()
